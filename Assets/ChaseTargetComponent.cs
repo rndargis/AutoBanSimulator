fa564ef5abe1ab8293d6abc12050fb0841e83d86
AnotherTone <45046833+AnotherTone@users.noreply.github.com>
2019-11-10 11:48:29 -0500

    AI
    -Waypoints (working)
    -NavMesh (test)
    -Pathfinding (test)

---
+++ Assets/ChaseTargetComponent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseTargetComponent : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject target;
+
+    private float elapsedTime = 0;
+
+    // Update is called once per frame
+    void Start()
+    {
+        if (agent != null && target != null)
+            agent.SetDestination(target.transform.position);
+    }
+
+    private void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime > 1)
+        {
+            agent.SetDestination(target.transform.position);
+            elapsedTime -= 1;
+        }
+    }
+}
