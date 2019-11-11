643fc9d0ac35838c490e472443c5ee1347c0c33d
rndargis <42740638+rndargis@users.noreply.github.com>
2019-11-11 16:34:04 -0500

    implementing the Interceptor as a unity Standard Asset car

--- Assets/Script/CenterOfMass.cs
+++ Assets/Script/CenterOfMass.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 public class CenterOfMass : MonoBehaviour
 {
     public Rigidbody Rb;
     // Start is called before the first frame update
     private void Awake()
     {
+
         var pos = transform.localPosition;
         Rb.centerOfMass = pos;
     }
@@ -16,7 +18,7 @@ public class CenterOfMass : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rb.drag = Rb.velocity.magnitude / 250; //250
-        Rb.angularDrag = Rb.velocity.magnitude / 250; //250  
+       // Rb.drag = Rb.velocity.magnitude / 250; //250
+       // Rb.angularDrag = Rb.velocity.magnitude / 250; //250  
     }
 }
